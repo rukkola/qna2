958be7963788fc8aec2927fd6bc589bfa7e34fb4
Serg <seregaproger@yandex.ru>
2020-11-06 20:24:17 +0300

    Polimorphic attachments. Answer associated with attachments

--- spec/acceptance/add_files_to_answer_spec.rb
+++ spec/acceptance/add_files_to_answer_spec.rb
@@ -1,24 +1,26 @@
 require_relative 'acceptance_helper'
 
-feature 'Add files to question', %q{
+feature 'Add files to answer', %q{
   In order to illustrate my question
 As an ques.. s autor
 I'd like to be able to attach files
 } do
 
   given(:user) { create(:user) }
+  given(:question) { create(:question) }
 
   background do
     sign_in(user)
-    visit new_question_path
+    visit question_path(question)
   end
 
   scenario 'User add files' do
-    fill_in 'Title', with: 'Test question'
-    fill_in 'Body', with: 'text text text'
+    fill_in 'Your answer', with: 'My answer'
     attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
     click_on 'Create'
 
-    expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+    within '.answer' do
+      expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+    end
   end
 end
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  #it { should have_many :answers } # непонятно зачем это
-
   it { should belong_to :question }
+  it { should have_many :attachments }
+
   it { should validate_presence_of :body }
+
+  it { should accept_nested_attributes_for :attachments }
 end
